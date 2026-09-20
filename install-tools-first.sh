e245d2e7aa30ddbc95543aecd760f5a203096fe0
Willie Shen <cg10250207@gmail.com>
2026-09-20 18:15:50 +0800

    2026-09-20T10:15:50+00:00

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -1390,7 +1390,6 @@ sudo systemctl enable --now ssh
 sudo ufw --force enable
 sudo ufw allow ssh
 sudo ufw reload
-octave -G -e 'pkg install -forge control'
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
 . "$HOME/.cargo/env"
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
