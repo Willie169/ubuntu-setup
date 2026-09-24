4dc2f2a6ab991b71fc80c48b8188cd6ca060a654
Willie Shen <cg10250207@gmail.com>
2026-09-25 00:22:33 +0800

    2026-09-24T16:22:33+00:00

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -1391,6 +1391,7 @@ sudo systemctl enable --now ssh
 sudo ufw --force enable
 sudo ufw allow ssh
 sudo ufw reload
+sudo systemctl enable --now i2p
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
 . "$HOME/.cargo/env"
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
