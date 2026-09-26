54eae83272927c34b73a46800bd0d471bd72a96c
Willie Shen <cg10250207@gmail.com>
2026-09-26 20:33:11 +0800

    2026-09-26T12:33:11+00:00

--- install-tools-first.sh
+++ install-tools-first.sh
@@ -1395,8 +1395,6 @@ sudo systemctl disable --now i2p
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
 . "$HOME/.cargo/env"
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-echo 'yes' | cpan
-cpan -i App::cpanminus
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
 sudo apt update
